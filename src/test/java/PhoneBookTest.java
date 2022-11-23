2f77374269fc8ef949b64ff3da93e2bdeaa82d9e
Amir Fazliev <fazliev.amir@yandex.ru>
2022-11-23 12:52:37 +0300

    Homework commit

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -5,60 +5,40 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.AnyOf.anyOf;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.hamcrest.core.IsNull.nullValue;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.hamcrest.object.HasToString.hasToString;
 
-//todo Домашнее задание к занятию 4.5. Коллекции TreeMap и TreeSet  - Пропущенные звонки
-
-public class MainTests {
+public class PhoneBookTest {
     PhoneBook phoneBookTest = new PhoneBook();
     Contact first = new Contact("first", "89");
-    Contact second =  new Contact("second", "89");
-    Contact third =  new Contact("third", "800");
-    Contact fourth = new Contact(null, "95");
+    Contact second = new Contact(null, "95");
 
-    @Test
-    public void testContactEqualsTrueInContact() {
-        Boolean expected = true;
-
-        boolean result = first.equals(second);
-        assertThat(expected, equalTo(result));
-    }
-
-    @Test
-    public void testContactEqualsFalseInContact() {
-        Boolean expected = false;
-
-        boolean result = first.equals(third);
-
-        assertThat(expected, equalTo(result));
-    }
 
     @Test
     public void testSeachContactNumberInPhonebookNull () {
         phoneBookTest.addPhoneBook(first.getNumberPhone(), first);
+        Contact expected = null;
 
         Contact result = phoneBookTest.seachContactNumber("88");
 
-        assertThat(result, nullValue());
+        assertThat(expected, equalTo(result));
     }
 
     @Test
     public void testSeachContactNumberInPhonebookNotNull () {
         phoneBookTest.addPhoneBook(first.getNumberPhone(), first);
+        Contact expected= first;
 
         Contact result = phoneBookTest.seachContactNumber("89");
 
-        assertThat(result, notNullValue());
+        assertThat(expected, equalTo(result));
     }
 
     @Test
     public void testInNullInPhonebookNull () {
-        String expected = fourth.getNumberPhone();
+        String expected = second.getNumberPhone();
 
-        String result = phoneBookTest.testInNull(fourth);
+        String result = phoneBookTest.testInNull(second);
 
         assertThat(expected, equalTo(result));
     }
@@ -85,8 +65,4 @@ public class MainTests {
 
         assertThat(result, hasToString("first - 89"));
     }
-
-
-
-
 }
